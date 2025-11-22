c698c107181fd049b80ec75aa9768998f7f23747
GiovanaJensen <oliveirasgiovana143@gmail.com>
2025-11-22 17:58:55 -0300

    FEAT: Tela de login

--- taskflow/src/app/shared/components/input/input.component.ts
+++ taskflow/src/app/shared/components/input/input.component.ts
@@ -1,12 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ]
 })
 export class InputComponent {
   @Input() type: string = 'text';
@@ -15,4 +23,29 @@ export class InputComponent {
   @Input() value?: string;
   @Input() name?: string;
   @Input() id?: string;
+
+  onChange = (value: any) => {};
+  onTouched = () => {};
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  updateValue(event: any) {
+    this.value = event.target.value;
+    this.onChange(this.value);
+    this.onTouched();
+  }
 }
